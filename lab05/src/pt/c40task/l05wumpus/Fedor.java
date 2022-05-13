d545aa11d2d70f9b68634c2983a7db0140ba1cb5
Giovanni <gamagiovanni10@gmail.com>
2022-05-13 15:45:05 -0300

    Adiciona a classe Fedor

---
+++ lab05/src/pt/c40task/l05wumpus/Fedor.java
@@ -0,0 +1,11 @@
+package pt.c40task.l05wumpus;
+
+public class Fedor extends Componente {
+    public Fedor(int[] pos, Caverna cav){
+        this.pos = new int[2];
+        this.pos[0] = pos[0];
+        this.pos[1] = pos[1];
+        this.cav = cav;
+        this.symbol = 'f';
+    }
+}
