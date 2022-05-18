b8c75a6f0b05e95209597b4aa3651179aff7da87
João Víctor <joaovictorso@hotmail.com.br>
2022-05-18 11:13:44 -0300

    adiciona instanciação de salas no construtor

--- lab05/src/pt/c40task/l05wumpus/Caverna.java
+++ lab05/src/pt/c40task/l05wumpus/Caverna.java
@@ -5,6 +5,13 @@ public class Caverna {
 
     public Caverna(){
         matriz = new Sala[4][4];
+
+        //adiciona novas salas à matriz de salas da caverna
+        for(int i=0; i<4; i++){
+            for(int j=0; j<4; j++){
+                matriz[i][j] = new Sala();
+            }
+        }
     }
 
     public Sala[][] getMatriz(){
