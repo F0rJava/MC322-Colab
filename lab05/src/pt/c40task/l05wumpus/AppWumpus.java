c2332bb4d9f89c4f621ad9797205a42f061cdb24
Giovanni <gamagiovanni10@gmail.com>
2022-05-19 13:24:23 -0300

    Modo interativo e impressao de comentários de acao

--- lab05/src/pt/c40task/l05wumpus/AppWumpus.java
+++ lab05/src/pt/c40task/l05wumpus/AppWumpus.java
@@ -16,8 +16,7 @@ public class AppWumpus {
       
       //recupera as entradas de caverna e movimentos dos arquivos
       String cave[][] = tk.retrieveCave();
-      String movements = tk.retrieveMovements();
-      
+
       //cria o objeto caverna
       Caverna caverna = new Caverna();
 
@@ -56,49 +55,82 @@ public class AppWumpus {
       System.out.println("Score: " + controle.getPontos());
       tk.writeBoard(impressaoCaverna, controle.getPontos(), controle.getStatus());
 
-      //imprime e registra a caverna a cada etapa
-      for(int i=0; i<movements.length(); i++){
-         boolean saiu = false;
-         if(movements.charAt(i) == 'c' ||movements.charAt(i) == 'k' || movements.charAt(i) == 'q'){
-            saiu = controle.acao(movements.charAt(i), saiu);
-            //checa se saiu e muda o status do controle
-            if(saiu && heroi.ouro == 1){
-               controle.setStatus('w');
-               break;
+      boolean saiu = false;
+      if(arquivoMovimentos == null){
+         //Modo interativo
+
+         while(!saiu){
+            int auxPontos = controle.getPontos();
+            Scanner keyboard = new Scanner(System.in);
+            String commands = keyboard.nextLine();
+            if(commands.equals("w") || commands.equals("a") || commands.equals("s") || commands.equals("d") || 
+               commands.equals("k") || commands.equals("c") || commands.equals("q")){
+               if(commands.charAt(0) == 'c' || commands.charAt(0) == 'k' || commands.charAt(0) == 'q')
+                  saiu = controle.acao(commands.charAt(0), saiu);
+               else
+                  controle.acao(commands.charAt(0));
+               imprimeCaverna(caverna, tk, impressaoCaverna, controle, saiu);
+               if(controle.getStatus() == 'L')
+                  saiu = true;
+               //imprime comentários de ação
+               if(commands.charAt(0) == 'k' && heroi.getFlecha() > 0)
+                  System.out.println("Flecha Equipada!");
+               if(commands.charAt(0) == 'c' && auxPontos < controle.getPontos())
+                  System.out.println("Ouro Pego!");   
             }
          }
-         else{
-            controle.acao(movements.charAt(i));
-         }
-
-         //imprime a caverna
-         for(int j=0; j<caverna.getMatriz().length; j++){
-            for(int k=0; k<caverna.getMatriz().length; k++){
-               impressaoCaverna[j][k] = caverna.getMatriz()[j][k].getPrioComponente();
-               System.out.printf("%c ", impressaoCaverna[j][k]);
-            }
-            System.out.printf("\n");
-         }
-         System.out.println("Player: Sting");
-         System.out.println("Score: " + controle.getPontos());
-
-         //imprime mensagens de fim de jogo: perdeu, venceu ou saiu do jogo
-         if(controle.getStatus()=='L'){
-            System.out.println("Voce perdeu =( ...");
-            break;
-         }
-         else if(controle.getStatus()=='W'){
-            System.out.println("Voce ganhou =D !!!");
-            break;
-         }
-         else if(saiu && controle.getStatus()=='P'){
-            System.out.println("Volte sempre !");
-            break;
-         }
-         tk.writeBoard(impressaoCaverna, controle.getPontos(), controle.getStatus());
       }
-      
+      else{
+         //Modo automático
+
+         //imprime e registra a caverna a cada etapa
+         String movements = tk.retrieveMovements();
+         for(int i=0; i<movements.length(); i++){
+            if(movements.charAt(i) == 'w' || movements.charAt(i) == 'a' || movements.charAt(i) == 's' || movements.charAt(i) == 'd' || 
+               movements.charAt(i) == 'c' || movements.charAt(i) == 'k' || movements.charAt(i) == 'q'){
+               int auxPontos = controle.getPontos();
+               if(movements.charAt(i) == 'c' ||movements.charAt(i) == 'k' || movements.charAt(i) == 'q')
+                  saiu = controle.acao(movements.charAt(i), saiu);
+               else
+                  controle.acao(movements.charAt(i));
+               imprimeCaverna(caverna, tk, impressaoCaverna, controle, saiu);
+               if(controle.getStatus() == 'L')
+                  saiu = true;
+               //imprime comentários de ação
+               if(movements.charAt(i) == 'k' && heroi.getFlecha() > 0)
+                  System.out.println("Flecha Equipada!");
+               if(movements.charAt(i) == 'c' && auxPontos < controle.getPontos())
+                  System.out.println("Ouro Pego!");
+            }   
+         }
+      }
       tk.stop();
    }
 
+   public static void imprimeCaverna(Caverna caverna, Toolkit tk, char[][] impressaoCaverna, Controle controle, boolean saiu){
+      for(int j=0; j<caverna.getMatriz().length; j++){
+         for(int k=0; k<caverna.getMatriz().length; k++){
+            impressaoCaverna[j][k] = caverna.getMatriz()[j][k].getPrioComponente();
+            System.out.printf("%c ", impressaoCaverna[j][k]);
+         }
+         System.out.printf("\n");
+      }
+      System.out.println("Player: Sting");
+      System.out.println("Score: " + controle.getPontos());
+
+      tk.writeBoard(impressaoCaverna, controle.getPontos(), controle.getStatus());      
+      //imprime mensagens de fim de jogo: perdeu, venceu ou saiu do jogo
+      if(controle.getStatus()=='L'){
+         System.out.println("Voce perdeu =( ...");
+         return;
+      }
+      else if(controle.getStatus()=='W'){
+         System.out.println("Voce ganhou =D !!!");
+         return;
+      }
+      else if(saiu && controle.getStatus()=='P'){
+         System.out.println("Volte sempre !");
+         return;
+      }
+   }
 }
