3d98591516cadb341a43602afd47137c0970a631
João Víctor <joaovictorso@hotmail.com.br>
2022-06-22 20:52:10 -0300

    Adiciona geração da fase

--- Crazychef/core/src/com/crazychef/game/ScreenLevel1.java
+++ Crazychef/core/src/com/crazychef/game/ScreenLevel1.java
@@ -1,5 +1,7 @@
 package com.crazychef.game;
 
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Null;
 import com.controller.Controller;
 import com.models.*;
 import com.badlogic.gdx.Gdx;
@@ -11,20 +13,26 @@ import com.badlogic.gdx.Input.*;
 
 public class ScreenLevel1 implements Screen{
     final Crazychef game;
-    Controller controller;
-    Texture chefFront;
-    Texture Background;
-    OrthographicCamera camera;
-    Chef chef;
+    private Controller controller;
+    private Texture chefFront;
+    private Texture background;
+    private Texture tableImageH;
+    private Texture tableImageV;
+    private OrthographicCamera camera;
+    private Chef chef;
+    private Objects objectsMatrix[][];
 
     public ScreenLevel1(final Crazychef game, Controller controller){
         this.game = game;
         this.controller = controller;
         chefFront = new Texture(Gdx.files.internal("Chef/chefFront.png"));//textura do chef
-        Background = new Texture(Gdx.files.internal("Kitchen/chao_fases.png"));//textura da fase
+        background = new Texture(Gdx.files.internal("Kitchen/chao_fases.png"));//textura da fase
+        tableImageH = new Texture(Gdx.files.internal("Kitchen/tableH.png"));
+        tableImageV = new Texture(Gdx.files.internal("Kitchen/tableV.png"));
 
         camera = new OrthographicCamera();//camera
         camera.setToOrtho(false, 1280, 720);
+        objectsMatrix = new Objects[9][16];
 
         chef = new Chef(640,320);
         chef.width = 80;
@@ -40,8 +48,16 @@ public class ScreenLevel1 implements Screen{
         game.batch.setProjectionMatrix(camera.combined);
 
         game.batch.begin(); //inicia a renderização
-        game.batch.draw(Background, 0,0, 1280, 720);//imagem do fundo
-        //game.batch.draw(chef, chef.x, chef.y);
+        game.batch.draw(background, 0,0, 1280, 720);//imagem do fundo
+        game.batch.draw(chefFront, chef.x, chef.y,chef.width,chef.height);
+
+        //cria o mapa da sala
+        for(int i=1; i<15;i++){
+            Table tableV = new Table(80*i,480);
+            objectsMatrix[2][i] = tableV;
+            game.batch.draw(tableImageV,tableV.x, tableV.y, tableV.width,tableV.height);
+        }
+
         game.batch.draw(chefFront, chef.x, chef.y,chef.width,chef.height);
         game.batch.end();
 
@@ -49,22 +65,27 @@ public class ScreenLevel1 implements Screen{
             controller.left();
         if (Gdx.input.isKeyPressed(Keys.RIGHT))
             controller.right();
-        if (Gdx.input.isKeyPressed(Keys.UP))
-            controller.up();
+        if (Gdx.input.isKeyPressed(Keys.UP)){
+            if(objectsMatrix[(int) (chef.y/80)][(int) (chef.x/80)+1]==null){
+                controller.up();
+            }
+        }
+
         if (Gdx.input.isKeyPressed(Keys.DOWN))
             controller.down();
 
         //nao deixa o chef sair das bordas da tela
         if (chef.x < 0)
             chef.x = 0;
-        if (chef.x > 1280 - 60)
-            chef.x = 1280 - 60;
-        if (chef.y > 720 - 60)
-            chef.y = 720 - 60;
+        if (chef.x > 1280 - 80)
+            chef.x = 1280 - 80;
+        if (chef.y > 720 - 120)
+            chef.y = 720 - 120;
         if (chef.y <0)
             chef.y = 0;
 
     }
+
 
     @Override
     public void resize(int width, int height) {
@@ -85,7 +106,7 @@ public class ScreenLevel1 implements Screen{
     @Override
     public void dispose() {
         chefFront.dispose();
-        Background.dispose();
+        background.dispose();
     }
 
     @Override
