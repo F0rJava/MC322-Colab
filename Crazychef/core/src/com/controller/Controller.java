cfab5ec1738397db93f9685eb751b94d6f6caee8
joaovsoliveira <joaovictorso@hotmail.com.br>
2022-06-23 10:23:36 -0300

    Adiciona funcionalidades ao controle

--- Crazychef/core/src/com/controller/Controller.java
+++ Crazychef/core/src/com/controller/Controller.java
@@ -17,19 +17,25 @@ import javax.swing.text.View;
 //classe responsavel por controlar as views do jogo e os models
 public class Controller{
     private Chef chef;
+    private Kitchen kitchen;
 
 
     //movimenta o chef, checando se não há colisões com outros objetos
-    public void up(){
-        chef.y += 80;
+    public void up(Objects[][] kitchen){
+        if(kitchen[Math.round(chef.y/80)+1][Math.round(chef.x/80)]==null && Math.round(chef.y/80)+1<9){
+            kitchen[Math.round(chef.y/80)][Math.round(chef.x/80)] = null; //remove o chefe da posição anterior
+            kitchen[Math.round(chef.y/80)][Math.round(chef.x/80)] = chef;//adiciona o chef na nova posição
+            chef.y += 80;
+        }
+
     }
-    public void down(){
+    public void down(Objects[][] kitchen){
         chef.y -= 80;
     }
-    public void left(){
+    public void left(Objects[][] kitchen){
         chef.x -= 80;
     }
-    public void right(){
+    public void right(Objects[][] kitchen){
         chef.x += 80;
     }
     public void setChef(Chef chef){
