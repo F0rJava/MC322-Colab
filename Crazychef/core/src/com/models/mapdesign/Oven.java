51bb29a6adaf1ee96b5c5509fb0fef9cc4748aac
Giovanni <gamagiovanni10@gmail.com>
2022-06-23 17:28:03 -0300

    [Feat] orientação do chef

---
+++ Crazychef/core/src/com/models/mapdesign/Oven.java
@@ -0,0 +1,11 @@
+package com.models.mapdesign;
+
+import com.models.Objects;
+
+public class Oven extends Objects {
+    public Oven(int x, int y){
+        super(x,y);
+        this.width = 80;
+        this.height = 80;
+    }
+}
