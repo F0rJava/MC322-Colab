448e75c98994a4e11e1a92bff641a76bd88bd229
Giovanni <gamagiovanni10@gmail.com>
2022-06-23 22:58:28 -0300

    Edita a classe Chef para já conter as texturas

--- Crazychef/core/src/com/models/Chef.java
+++ Crazychef/core/src/com/models/Chef.java
@@ -1,22 +1,39 @@
 package com.models;
 
+import com.badlogic.gdx.Gdx;
 import com.models.mapdesign.Plate;
+import com.badlogic.gdx.graphics.Texture;
+
+import java.io.ObjectStreamException;
 
 public class Chef extends Objects {
     //private ArrayList;
     private Plate plate;
     private boolean[] orientation;
 
+    //texturas do chef
+    private Texture chefFront;
+    private Texture chefBack;
+    private Texture chefLeft;
+    private Texture chefRight;
+
     public Chef(int x, int y) {
         //posição onde o chefe inicia
         super(x,y);
         this.width = 80;
         this.height = 140;
+        this.plate = null;
+
         orientation = new boolean[4];
         orientation[0] = true; //chefFront
         orientation[1] = false;//chefBack
         orientation[2] = false;//chefLeft
         orientation[3] = false;//chefRight
+
+        chefFront = new Texture(Gdx.files.internal("Chef/chefFront.png"));//textura do chef
+        chefBack = new Texture(Gdx.files.internal("Chef/chefBack.png"));
+        chefRight = new Texture(Gdx.files.internal("Chef/chefRight.png"));
+        chefLeft = new Texture(Gdx.files.internal("Chef/chefLeft.png"));
     }
 
     public void setOrientation(int i){
@@ -29,5 +46,16 @@ public class Chef extends Objects {
 
     public boolean getOrientation(int i){
         return orientation[i];
+    }
+
+    public Texture getTexture(){
+        if(orientation[0])
+            return chefFront;
+        else if(orientation[1])
+            return chefBack;
+        else if(orientation[2])
+            return chefLeft;
+        else
+            return chefRight;
     }
 }
